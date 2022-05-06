140bf73c9b96c21e18e342d60e6141d7949148f8
Arnold <arnold.gonczlik@hotmail.com>
2022-05-06 15:48:45 +0200

    Compression task B work

---
+++ exercises/src/main/java/org/pg4200/ex11/testv2.java
@@ -0,0 +1,16 @@
+package org.pg4200.ex11;
+
+public class testv2 {
+    public static void main(String[] args) {
+        String test = "x1";
+
+        char b = test.charAt(1);
+
+        if ((""+b).equals("2")) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
